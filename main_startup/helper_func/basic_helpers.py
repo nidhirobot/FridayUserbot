d9af066856a187441f037081f68d7c9ec0cc7c72
スタークギャング <StarkGangZ@gmail.com>
2021-04-27 12:41:51 +0530

    Update basic_helpers.py

--- main_startup/helper_func/basic_helpers.py
+++ main_startup/helper_func/basic_helpers.py
@@ -63,7 +63,7 @@ async def edit_or_reply(message, text, parse_mode="md"):
         return await message.edit(text, parse_mode=parse_mode)
     if not message.from_user:
         return await message.edit(text, parse_mode=parse_mode)
-    if message.client.is_bot:
+    if message.client.me.is_bot:
         if message.reply_to_message:
             kk = message.reply_to_message.message_id
             return await message.reply(
